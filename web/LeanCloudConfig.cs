3594eedb854a3244105f927ba038464fedfef7ab
yangchao <117412555@qq.com>
2025-09-08 13:18:40 +0800

    1

--- web/LeanCloudConfig.cs
+++ web/LeanCloudConfig.cs
@@ -17,9 +17,9 @@ namespace web
             string appId = Environment.GetEnvironmentVariable("APP_ID");
             string appKey = Environment.GetEnvironmentVariable("APP_KEY");
             string masterKey = Environment.GetEnvironmentVariable("MASTER_KEY");
+            string appUrl = Environment.GetEnvironmentVariable("APP_URL");
 
-
-            AVClient.Initialize(appId, appKey);
+            AVClient.Initialize(appId, appKey, appUrl);
             AVClient.CurrentConfiguration.MasterKey = masterKey;
             AVClient.UseMasterKey = true;
             Console.WriteLine("≈‰÷√≥…π¶!!!");
