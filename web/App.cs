dafbad62d50cf9bec5076349013e793f3d2ee27a
yangchao <117412555@qq.com>
2025-09-09 17:41:08 +0800

    1

--- web/App.cs
+++ web/App.cs
@@ -2,15 +2,37 @@
 using LeanCloud.Engine;
 using LeanCloud.Push;
 using LeanCloud.Storage;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Threading.Tasks;
 
 namespace web {
     public class App
     {
+        // 定义接收 Hook 负载（Payload）的数据结构
+        // LeanCloud 会发送一个 JSON 对象，包含以下字段
+        public class ClientStatusEventPayload
+        {
+            [JsonProperty("clientId")]
+            public string ClientId { get; set; } // 上下线的客户端 ID
+
+            [JsonProperty("sessionToken")]
+            public string SessionToken { get; set; } // 会话 Token（传统字段）
+
+            [JsonProperty("deviceId")]
+            public string DeviceId { get; set; } // 设备标识（新字段，更推荐）
+
+            [JsonProperty("reason")]
+            public string Reason { get; set; } // 仅 _clientOffline 有，断开原因
+        }
+
         // Function
         [LCEngineFunction("hello")]
         public static string Hello([LCEngineFunctionParam("name")] string name)
@@ -68,22 +90,19 @@ namespace web {
 
 
         [LCEngineFunction("_clientOffline")]
-        public static void ClientOffLine(LCUser user)
+        public static void ClientOffLine([FromBody] ClientStatusEventPayload payload)
         {
             Console.WriteLine("start execute: ClientOffLine");
-            Console.WriteLine(user.ObjectId);
+            Console.WriteLine(payload.ClientId);
             Console.WriteLine("end execute: ClientOffLine");
         }
+
         [LCEngineFunction("_clientOnline")]
-        public static void ClientOnLine(LCUser user)
+        public static void ClientOnLine([FromBody] ClientStatusEventPayload payload)
         {
             Console.WriteLine("start execute: ClientOnLine");
-            Console.WriteLine(user.ObjectId);
+            Console.WriteLine(payload.ClientId);
             Console.WriteLine("end execute: ClientOnLine");
-        }
-
-        public static async Task Onddd() { 
-        
         }
     }
 }
