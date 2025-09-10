8d6d72c73784273724b079d42f1f935a9c897c9e
yangchao <117412555@qq.com>
2025-09-10 16:08:54 +0800

    cccccccccccc

--- web/Models/ClientMessageBase.cs
+++ web/Models/ClientMessageBase.cs
@@ -1,6 +1,7 @@
 using LeanCloud;
 using LeanCloud.Core.Internal;
 using LeanCloud.Realtime;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -9,12 +10,12 @@ public class ClientMessageBase : AVIMTypedMessage
 {
     public async Task Save() 
     {
-        //JObject jsonObject = JObject.Parse(Content);
-        //var content = jsonObject.ToObject<Dictionary<string, object>>();
-
         var table = new AVObject("customMessage");
         table.Set("IsProcessed", false);
-        table.Set("content", Content);
+
+        JObject jsonObject = JObject.Parse(Content);
+        var content = jsonObject.ToObject<Dictionary<string, object>>();
+        table.Set("content", content);
         await table.SaveAsync();
     }
 
