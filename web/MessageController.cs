24e8526886e3b536b7b8e20b091c3cfcbd70c737
super <117412555@qq.com>
2025-09-11 22:31:39 +0800

    1

--- web/MessageController.cs
+++ web/MessageController.cs
@@ -1,37 +1,37 @@
-using Microsoft.AspNetCore.Mvc;
-using System.Threading.Tasks;
+//using Microsoft.AspNetCore.Mvc;
+//using System.Threading.Tasks;
 
-[ApiController]
-[Route("api/[controller]")]
-public class MessageController : ControllerBase
-{
-    private readonly HttpClientIMService _messageService;
+//[ApiController]
+//[Route("api/[controller]")]
+//public class MessageController : ControllerBase
+//{
+//    private readonly HttpClientIMService _messageService;
     
-    public MessageController(HttpClientIMService messageService)
-    {
-        _messageService = messageService;
-    }
+//    public MessageController(HttpClientIMService messageService)
+//    {
+//        _messageService = messageService;
+//    }
     
-    [HttpPost("send")]
-    public async Task<IActionResult> SendMessage([FromBody] MessageRequest request)
-    {
-        var result = await _messageService.SendMessageToClientAsync(
-            request.TargetClientId,
-            request.MessageContent);
+//    [HttpPost("send")]
+//    public async Task<IActionResult> SendMessage([FromBody] MessageRequest request)
+//    {
+//        var result = await _messageService.SendMessageToClientAsync(
+//            request.TargetClientId,
+//            request.MessageContent);
 
-        if (result)
-        {
-            return Ok(new { success = true, message = "消息发送成功" });
-        }
-        else
-        {
-            return BadRequest(new { success = false, message = "消息发送失败" });
-        }
-    }
-}
+//        if (result)
+//        {
+//            return Ok(new { success = true, message = "消息发送成功" });
+//        }
+//        else
+//        {
+//            return BadRequest(new { success = false, message = "消息发送失败" });
+//        }
+//    }
+//}
 
-public class MessageRequest
-{
-    public string TargetClientId { get; set; }
-    public string MessageContent { get; set; }
-}
+//public class MessageRequest
+//{
+//    public string TargetClientId { get; set; }
+//    public string MessageContent { get; set; }
+//}
