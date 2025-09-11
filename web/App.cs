391bf29fbf7d5fdd1d397ed2de2b30460459dedc
yangchao <117412555@qq.com>
2025-09-11 19:28:03 +0800

    t

--- web/App.cs
+++ web/App.cs
@@ -12,19 +12,19 @@ namespace web {
     {
         public static HttpClientIMService _httpClient;
 
-        // Function
-        [LCEngineFunction("hello")]
-        public static string Hello([LCEngineFunctionParam("name")] string name)
-        {
-            string msg = $"hello, {name}";
-            Console.WriteLine(msg);
-            return msg;
-        }
-        [LCEngineFunction("TestCloudFunc")]
-        public static string TestCloudFunc([LCEngineFunctionParam("name")] string name)
-        {
-            return "TestCloudFunc:" + name;
-        }
+        //// Function
+        //[LCEngineFunction("hello")]
+        //public static string Hello([LCEngineFunctionParam("name")] string name)
+        //{
+        //    string msg = $"hello, {name}";
+        //    Console.WriteLine(msg);
+        //    return msg;
+        //}
+        //[LCEngineFunction("TestCloudFunc")]
+        //public static string TestCloudFunc([LCEngineFunctionParam("name")] string name)
+        //{
+        //    return "TestCloudFunc:" + name;
+        //}
 
 
         [LCEngineFunction("SendMessageToTargetUserID")]
