a58c128683fd0282f0cb0134dc66b5c08081ecd7
yangchao <117412555@qq.com>
2025-09-11 14:13:22 +0800

    注册服务

--- web/Startup.cs
+++ web/Startup.cs
@@ -50,6 +50,8 @@ namespace web {
             LCEngine.Initialize(services);
 
             services.AddControllersWithViews();
+
+            services.AddSingleton<MeeeageServicr>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
