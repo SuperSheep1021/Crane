3b8195d455d562e83964d1bcacfa8083f05b817f
yangchao <117412555@qq.com>
2025-09-11 12:19:13 +0800

    aewrq

--- web/LeanCloudConfig.cs
+++ web/LeanCloudConfig.cs
@@ -1,11 +1,7 @@
 using LeanCloud;
 using LeanCloud.Common;
 using LeanCloud.Realtime;
-using LeanCloud.Storage.Internal;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
+using LeanCloud.Storage;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -35,35 +31,35 @@ namespace web
             LCLogger.Debug($"Config Success!!!{appUrl}");
         }
 
-        public static async Task<bool> SendToSingleUser(string targetUserId, Dictionary<string, object> content)
-        {
-            if (m_SysClient == null)
-            {
-                m_SysClient = await m_StyRealtime.CreateClientAsync(SystemClientId, tag: "StyemBroadcast");
-            }
-            if (m_SysClient == null)
-            {
-                return false;
-            }
+        //public static async Task<bool> SendToSingleUser(string targetUserId, Dictionary<string, object> content)
+        //{
+        //    if (m_SysClient == null)
+        //    {
+        //        m_SysClient = await m_StyRealtime.CreateClientAsync(SystemClientId, tag: "StyemBroadcast");
+        //    }
+        //    if (m_SysClient == null)
+        //    {
+        //        return false;
+        //    }
 
-            try
-            {
-                //AVIMConversation conversation2 = await m_SysClient.GetConversationAsync(SystemConversationId);
-                AVIMConversation conversation = await m_SysClient.CreateConversationAsync(member: targetUserId, isSystem: true, isUnique: true);
+        //    try
+        //    {
+        //        //AVIMConversation conversation2 = await m_SysClient.GetConversationAsync(SystemConversationId);
+        //        AVIMConversation conversation = await m_SysClient.CreateConversationAsync(member: targetUserId, isSystem: true, isUnique: true);
 
-                // 发送消息
-                var message = new AVIMTextMessage("StyMessage");
-                message.Content = Json.Encode(content);
+        //        // 发送消息
+        //        var message = new AVIMTextMessage("StyMessage");
+        //        message.Content = Json.Encode(content);
 
-                await conversation.SendAsync(message);
-                return true;
-            }
-            catch (LCException ex)
-            {
-                LCLogger.Debug($"给用户 {targetUserId} 发送消息失败: {ex.Message}");
-                return false;
-            }
-        }
+        //        await conversation.SendAsync(message);
+        //        return true;
+        //    }
+        //    catch (LCException ex)
+        //    {
+        //        LCLogger.Debug($"给用户 {targetUserId} 发送消息失败: {ex.Message}");
+        //        return false;
+        //    }
+        //}
 
     }
 }
