3cd3d8d2c4312ae4842d6867ecf0110077830839
yangchao <117412555@qq.com>
2025-09-11 18:42:01 +0800

    sadw

--- web/App.cs
+++ web/App.cs
@@ -125,26 +125,26 @@ namespace web {
         }
 
 
-        //[LCEngineRealtimeHook(LCEngineRealtimeHookType.ClientOnline)]
-        //public static async Task<object> ClientOnLine(dynamic request)
-        //{
-        //    string data = JsonConvert.SerializeObject(request);
-        //    LCLogger.Debug($"客户端上线: {request}");
-        //    try
-        //    {
-        //        var dic = request;
-        //        foreach (KeyValuePair<string, object> item in dic)
-        //        {
-        //            LCLogger.Debug(item.Key + ":" + item.Value);
-        //        }
+        [LCEngineRealtimeHook(LCEngineRealtimeHookType.ClientOnline)]
+        public static async Task<object> ClientOnLine(dynamic request)
+        {
+            string data = JsonConvert.SerializeObject(request);
+            LCLogger.Debug($"客户端上线: {data}");
+            try
+            {
+                var dic = request;
+                foreach (KeyValuePair<string, object> item in dic)
+                {
+                    LCLogger.Debug(item.Key + ":" + item.Value);
+                }
 
-        //    }
-        //    catch (LCException ex)
-        //    {
-        //        LCLogger.Error(ex.Message);
-        //    }
-        //    return new { success = true };
-        //}
+            }
+            catch (LCException ex)
+            {
+                LCLogger.Error(ex.Message);
+            }
+            return new { success = true };
+        }
 
 
 
