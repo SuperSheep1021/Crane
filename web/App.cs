5cab7b08192e0413b178893173297e3421bc95e3
super <117412555@qq.com>
2025-09-11 23:43:32 +0800

    1

--- web/App.cs
+++ web/App.cs
@@ -28,9 +28,10 @@ namespace web {
 
 
         [LCEngineFunction("SendMessageToTargetUserID")]
-        public static async void SendMessageToTargetUserID()
+        public static async void SendMessageToTargetUserID([LCEngineFunctionParam("name")] string name)
         {
             await _httpClient.SendToUser("68c22ec62f7ee809fcc9e7e6", "68b9286c49adb47c41678afb", "服务端消息发送");
+            LCLogger.Debug(name);
             //bool succes = await MeeeageServicr.Inst.CreateImClientAsync();
             //if (succes)
             //{
