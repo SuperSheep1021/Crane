2381c0168ddca49473d13a718d4350f30ab5c8bf
super <117412555@qq.com>
2025-09-14 22:35:38 +0800

    1

--- web/Program.cs
+++ web/Program.cs
@@ -14,17 +14,20 @@ namespace web {
             //LeanCloudConfig.InitializeFromEnvironment();
             //CreateHostBuilder(args).Build().Run();
 
+
+            LCLogger.Debug($"Start Main");
+
             ////// 从环境变量获取配置信息
             string appId = Environment.GetEnvironmentVariable("APP_ID");
             string appKey = Environment.GetEnvironmentVariable("APP_KEY");
             string appUrl = Environment.GetEnvironmentVariable("APP_URL");
             string masterKey = Environment.GetEnvironmentVariable("MASTER_KEY");
 
-            LCCore.UseMasterKey = true;
-            LCCore.Initialize(appId, appKey, appUrl, masterKey);
+            //LCCore.UseMasterKey = true;
+            //LCCore.Initialize(appId, appKey, appUrl, masterKey);
             //LCStorage.Initialize(appId, appKey, appUrl, masterKey);
             //LCCore.PersistenceController = new PersistenceController(new UnityPersistence());
-            //LCApplication.Initialize(appId, appKey, appUrl, masterKey);
+            LCApplication.Initialize(appId, appKey, appUrl, masterKey);
             LCLogger.Debug($"LCApplication.Initialize Success URL:{appUrl}");
 
             CreateHostBuilder(args).Build().Run();
