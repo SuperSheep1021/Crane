27c07247df3cedb5d287762c99bdbbfdcc55a248
yangchao <117412555@qq.com>
2025-09-15 17:02:07 +0800

    ssssssssssssssssss

--- web/Services/SysClientService.cs
+++ web/Services/SysClientService.cs
@@ -113,7 +113,7 @@ public class SysClientService
         // 构建请求数据（添加用户到订阅者列表）
         var requestData = new Dictionary<string, object>
         {
-            { "client_ids", $"{userId}" }
+            { "client_id", $"{userId}" }
         };
 
         //var jsonData = JsonConvert.SerializeObject(requestData);
@@ -125,7 +125,7 @@ public class SysClientService
         {
             // 例如添加认证信息或其他必要头信息
             { "X-LC-Key",$"{Environment.GetEnvironmentVariable("MASTER_KEY")},master" },
-            { "client_ids", $"{userId}" }
+            { "client_id", $"{userId}" }
         };
 
         // 调用Post方法发送请求
